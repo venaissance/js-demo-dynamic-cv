a2aac2615de553a665193f598140b01bddc62bcb
varian <mizeyuyu@gmail.com>
2019-10-28 20:23:26 +0800

    PC+手机端页面第一版开发完成。

--- dist/style.e308ff8e.js
+++ dist/style.e308ff8e.js
@@ -117,40 +117,79 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"main.js":[function(require,module,exports) {
-var html = document.querySelector("#html");
-var style = document.querySelector("#style");
-var string = "/* \u4F60\u597D\uFF0C\u6211\u53EB\u7C73\u5927\u738B\n * \u63A5\u4E0B\u6765\u6211\u8981\u5C55\u793A\u4E00\u4E0B\u771F\u6B63\u7684\u524D\u7AEF\u6280\u672F\u4E86\n * \u9996\u5148\u8981\u51C6\u5907\u4E00\u4E2Adiv\n **/\n#div1 {\n    border: 1px solid red;\n    width: 200px;\n    height: 200px\n}\n/* \u63A5\u4E0B\u6765\u6211\u628A div \u53D8\u6210\u4E00\u4E2A\u592A\u6781\u516B\u5366\u56FE\n * \u6CE8\u610F\u770B\u597D\u4E86\n * \u9996\u5148\uFF0C\u628A div \u53D8\u6210\u4E00\u4E2A\u5706\n**/\n#div1 {\n    border-radius: 50%;\n    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);\n    border: none;\n}\n/* \u516B\u5366\u662F\u9634\u9633\u5F62\u6210\u7684\n * \u4E00\u9ED1\u4E00\u767D\n **/\n#div1 {\n    background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 50%, rgba(0,0,0,1) 50%, rgba(0,0,0,1) 100%);\n}\n/* \u73B0\u5728\u52A0\u4E24\u4E2A\u5C0F\u7403 */\n#div1::before {\n    width: 100px;\n    height: 100px;\n    top:0;\n    left: 50%;\n    transform: translateX(-50%);\n    background: #000;\n    border-radius: 50%;\n    background: radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 25%, rgba(0,0,0,1) 25%, rgba(0,0,0,1) 100%);\n}\n#div1::after {\n    width: 100px;\n    height: 100px;\n    bottom:0;\n    left: 50%;\n    transform: translateX(-50%);\n    background: #fff;\n    border-radius: 50%;\n    background: radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 25%, rgba(255,255,255,1) 25%, rgba(255,255,255,1) 100%);\n}\n";
-var string2 = "";
-var n = -1;
+})({"../../../../.config/yarn/global/node_modules/parcel/src/builtins/bundle-url.js":[function(require,module,exports) {
+var bundleURL = null;
 
-var step = function step() {
-  setTimeout(function () {
-    n += 1;
+function getBundleURLCached() {
+  if (!bundleURL) {
+    bundleURL = getBundleURL();
+  }
 
-    if (string[n] === "\n") {
-      string2 += "<br>"; // 换行的HTML表示
-    } else if (string[n] === " ") {
-      string2 += "&nbsp"; // 空格的HTML表示
-    } else {
-      string2 += string[n];
+  return bundleURL;
+}
+
+function getBundleURL() {
+  // Attempt to find the URL of the current script and use that as the base URL
+  try {
+    throw new Error();
+  } catch (err) {
+    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
+
+    if (matches) {
+      return getBaseURL(matches[0]);
+    }
+  }
+
+  return '/';
+}
+
+function getBaseURL(url) {
+  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)\/[^/]+$/, '$1') + '/';
+}
+
+exports.getBundleURL = getBundleURLCached;
+exports.getBaseURL = getBaseURL;
+},{}],"../../../../.config/yarn/global/node_modules/parcel/src/builtins/css-loader.js":[function(require,module,exports) {
+var bundle = require('./bundle-url');
+
+function updateLink(link) {
+  var newLink = link.cloneNode();
+
+  newLink.onload = function () {
+    link.remove();
+  };
+
+  newLink.href = link.href.split('?')[0] + '?' + Date.now();
+  link.parentNode.insertBefore(newLink, link.nextSibling);
+}
+
+var cssTimeout = null;
+
+function reloadCSS() {
+  if (cssTimeout) {
+    return;
+  }
+
+  cssTimeout = setTimeout(function () {
+    var links = document.querySelectorAll('link[rel="stylesheet"]');
+
+    for (var i = 0; i < links.length; i++) {
+      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
+        updateLink(links[i]);
+      }
     }
 
-    html.innerHTML = string2;
-    style.innerHTML = string.substring(0, n); // 发挥CSS的功效
+    cssTimeout = null;
+  }, 50);
+}
 
-    window.scrollTo(0, 99999); // 每刷新一次，滚动到屏幕最底部
+module.exports = reloadCSS;
+},{"./bundle-url":"../../../../.config/yarn/global/node_modules/parcel/src/builtins/bundle-url.js"}],"style.css":[function(require,module,exports) {
+var reloadCSS = require('_css_loader');
 
-    html.scrollTo(0, 99999); // 手机页面上下布局时，使overflow为auto的div1能滚到最下面
-
-    if (n + 1 < string.length) {
-      step();
-    }
-  }, 0.1);
-};
-
-step();
-},{}],"../../../../.config/yarn/global/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+module.hot.dispose(reloadCSS);
+module.hot.accept(reloadCSS);
+},{"_css_loader":"../../../../.config/yarn/global/node_modules/parcel/src/builtins/css-loader.js"}],"../../../../.config/yarn/global/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -354,5 +393,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../../../.config/yarn/global/node_modules/parcel/src/builtins/hmr-runtime.js","main.js"], null)
-//# sourceMappingURL=/main.1f19ae8e.js.map
+},{}]},{},["../../../../.config/yarn/global/node_modules/parcel/src/builtins/hmr-runtime.js"], null)
+//# sourceMappingURL=/style.e308ff8e.js.map
--- src/main.js
+++ src/main.js
@@ -1,17 +1,74 @@
-let demo = document.querySelector("#demo");
-let string = `今天，我陪我最爱的小宝贝来考试。`;
-let n = 0;
+let html = document.querySelector("#html");
+let style = document.querySelector("#style");
 
-demo.innerHTML = string.substring(0, n);
+let string = `/* 你好，我叫米大王
+ * 接下来我要展示一下真正的前端技术了
+ * 首先要准备一个div
+ **/
+#div1 {
+    border: 1px solid red;
+    width: 200px;
+    height: 200px
+}
+/* 接下来我把 div 变成一个太极八卦图
+ * 注意看好了
+ * 首先，把 div 变成一个圆
+**/
+#div1 {
+    border-radius: 50%;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+    border: none;
+}
+/* 八卦是阴阳形成的
+ * 一黑一白
+ **/
+#div1 {
+    background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 50%, rgba(0,0,0,1) 50%, rgba(0,0,0,1) 100%);
+}
+/* 现在加两个小球 */
+#div1::before {
+    width: 100px;
+    height: 100px;
+    top:0;
+    left: 50%;
+    transform: translateX(-50%);
+    background: #000;
+    border-radius: 50%;
+    background: radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 25%, rgba(0,0,0,1) 25%, rgba(0,0,0,1) 100%);
+}
+#div1::after {
+    width: 100px;
+    height: 100px;
+    bottom:0;
+    left: 50%;
+    transform: translateX(-50%);
+    background: #fff;
+    border-radius: 50%;
+    background: radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 25%, rgba(255,255,255,1) 25%, rgba(255,255,255,1) 100%);
+}
+`;
+let string2 = "";
+
+let n = -1;
 
 let step = () => {
   setTimeout(() => {
     n += 1;
-    demo.innerHTML = string.substring(0, n);
-    if (n < string.length) {
+    if (string[n] === "\n") {
+      string2 += "<br>"; // 换行的HTML表示
+    } else if (string[n] === " ") {
+      string2 += "&nbsp"; // 空格的HTML表示
+    } else {
+      string2 += string[n];
+    }
+    html.innerHTML = string2;
+    style.innerHTML = string.substring(0, n); // 发挥CSS的功效
+    window.scrollTo(0, 99999); // 每刷新一次，滚动到屏幕最底部
+    html.scrollTo(0, 99999); // 手机页面上下布局时，使overflow为auto的div1能滚到最下面
+    if (n + 1 < string.length) {
       step();
     }
-  }, 200);
+  }, 0.1);
 };
 
 step();
